f3b335ac29d4d8b18e74e725c8ccbb171a4334ce
Matthew Foote <matthew.foote@gmail.com>
2025-07-06 05:24:04 -0700

    Cleanup and implement docker setup and backup

---
+++ zz-archive/Windows/Config Files/keypirinha/files/Packages/Bookmarks/providers/__init__.py
@@ -0,0 +1,11 @@
+# Keypirinha: a fast launcher for Windows (keypirinha.com)
+
+from ._base import Bookmark
+from .chrome import ChromeProvider, ChromeCanaryProvider, ChromiumProvider
+from .edge import EdgeProvider
+from .falkon import FalkonProvider
+from .firefox import FirefoxProvider
+from .iexplorer import InternetExplorerProvider
+from .iridium import IridiumProvider
+from .vivaldi import VivaldiProvider
+from .buku import BukuProvider
